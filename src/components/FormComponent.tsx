1c92a9661ce81fbfca7506b4101ac09a5b039f96
Uliia17 <45678julia@gmail.com>
2025-01-19 18:22:33 +0200

    Lesson5.2

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,9 +1,25 @@
+import {useForm} from "react-hook-form";
+
+interface IFormProps {
+    username:string,
+    password:string,
+    age:number
+}
+
 const FormComponent = () => {
+    const {handleSubmit, register} = useForm<IFormProps>();
+    // console.log(useForm1);
+    console.log(register('username'));
+
     return (
         <div>
-            <form>
-                <input type="text" name={'username'}/>
-                <input type="text" name={'password'}/>
+            <form onSubmit={handleSubmit((formData) => {
+                console.log(formData);
+            })}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <input type="number" {...register('age')}/>
+                <button>send</button>
             </form>
         </div>
     );
